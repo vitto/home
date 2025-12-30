e2f289ef6099a67e1b035d55f43d48ee6e706bc9
Vittorio Vittori <vittorio.vittori@maggioli.it>
2025-12-30 12:28:38 +0100

    chore: update iconsauce configuration to remove unused prefix and adjust output path for SVG assets

--- iconsauce.config.cjs
+++ iconsauce.config.cjs
@@ -4,14 +4,5 @@ module.exports = {
     require('@iconsauce/material-icons'),
     require('@iconsauce/mdi-svg'),
     require('@iconsauce/mgg-icons'),
-    {
-      prefix: 'vv',
-      regex: {
-        code: /(vv)(\/{1}[0-9a-z-]+){1}/gm,
-        lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
-      },
-      selector: (regexLib) => `vv/${regexLib[2].replace(/[_]+/g, '-')}`,
-      path: './src/icons/*.svg',
-    },
   ],
 };
