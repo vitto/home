72756cb302d3411f205f793e3d90e1e76416ed44
Vittorio Vittori <vittorio.vittori@maggioli.it>
2026-01-02 07:05:44 +0100

    feat: add custom SVG plugin and new logo icon to iconsauce configuration

--- iconsauce.config.cjs
+++ iconsauce.config.cjs
@@ -1,8 +1,19 @@
+const customPlugin = {
+  prefix: 'vv',
+  regex: {
+    code: /(vv)(\/{1}[0-9a-z-]+){1}/gm,
+    lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
+  },
+  selector: (path) => `vv/${path[2].replace(/[_]+/g, '-')}`,
+  path: './src/svg/*.svg',
+};
+
 module.exports = {
   content: ['./src/**/*.{astro,js,json,ts,tsx,html}'],
   plugin: [
     require('@iconsauce/material-icons'),
     require('@iconsauce/mdi-svg'),
     require('@iconsauce/mgg-icons'),
+    customPlugin,
   ],
 };
