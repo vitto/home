40af71fceeb5851ff2620a625a452e69221900f5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2026-01-08 08:26:35 +0100

    feat: add new animations and keyframes for blink and pulse effects in Tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,8 @@ export default {
     extend: {
       animation: {
         'pulse-more': 'pulseMore 5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        blink: 'blink 0.6s forwards',
+        'pulse-group': 'pulseGroup 5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
       transitionProperty: {
         cosmetic:
@@ -17,6 +19,20 @@ export default {
         pulseMore: {
           '0%, 100%': { opacity: '1' },
           '50%': { opacity: '0.05' },
+        },
+        pulseGroup: {
+          '0%, 100%': {
+            borderColor: 'rgb(var(--tone-neutral-04) / 1)',
+            fill: 'rgb(var(--tone-neutral-04) / 1)',
+          },
+          '50%': {
+            borderColor: 'rgb(var(--tone-neutral-04) / 0.05)',
+            fill: 'rgb(var(--tone-neutral-04) / 0.05)',
+          },
+        },
+        blink: {
+          '0%, 50%': { opacity: '0' },
+          '50%, 100%': { opacity: '1' },
         },
       },
     },
