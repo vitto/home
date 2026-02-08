3759f5bb8c6d3a1d032ed6330d4fbc5c21ca6fa6
Vittorio Vittori <vittorio.vittori@maggioli.it>
2026-02-08 10:22:14 +0100

    feat: improve theme preference handling in PreventAnimation

--- src/utils/preferences.ts
+++ src/utils/preferences.ts
@@ -4,6 +4,7 @@ export function getPreferences(cookies: AstroCookies) {
   const prefTheme = cookies.get('mdsPrefTheme')?.value || 'system';
   return {
     theme: {
+      value: prefTheme,
       style: `--magma-pref-theme: ${prefTheme};`,
       selector: `pref-theme-${prefTheme}`,
     },
